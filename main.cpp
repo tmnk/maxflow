3b17658112b911c18bcc02361c506e713d15eca6
tmnk <temnikovgiga@yandex.ru>
2017-03-04 10:21:28 +0300

    change comm

--- main.cpp
+++ main.cpp
@@ -1,61 +1,56 @@
 #include <iostream>
-using namespace std;
+#include <limits.h>
 
-const int MAX_EDGE = 10000;
-const int MAX_VERTEX = 2000;
-const int MAX_WEIGHT   = 1000000000;
+const unsigned int MAX_VERTEX =       4004;
+const unsigned int MAX_EDGE   =      20004;
+const unsigned int MAX_WEIGHT =  UINT_MAX - 1;
+unsigned int numOfVertex, numOfEdge, sourceVertex, destinationVertex, edgeCount;
+unsigned int capacity[MAX_EDGE], onEnd[MAX_EDGE], nextEdge[MAX_EDGE], firstEdge[MAX_VERTEX], visited[MAX_VERTEX];
 
-int numOfVertex, numOfEdge, sourceVertex, destinationVertex;
-int capacity[MAX_EDGE], onEnd[MAX_EDGE], nextEdge[MAX_EDGE], edgeCount;
-int firstEdge[MAX_VERTEX], visited[MAX_VERTEX];
-
-void addEdge(int u, int v, int cap) {
-    // Прямое ребро
-    onEnd[edgeCount] = v;                   // на конце прямого v
-    nextEdge[edgeCount] = firstEdge[u];     // добавляем в начало списка для u
-    firstEdge[u] = edgeCount;               // теперь начало списка - новое ребро
-    capacity[edgeCount++] = cap;            // устанавливаем пропускную способность
-    // Обратное ребро
-    onEnd[edgeCount] = u;                   // на конце обратного u
-    nextEdge[edgeCount] = firstEdge[v];     // добавляем в начало списка для v
-    firstEdge[v] = edgeCount;               // теперь начало списка - новое ребро
-    capacity[edgeCount++] = 0;              // устанавливаем пропускную способность
+void addEdge(unsigned int u, unsigned int v, unsigned int cap) {
+    onEnd[edgeCount] = v;
+    nextEdge[edgeCount] = firstEdge[u];
+    firstEdge[u] = edgeCount;
+    capacity[edgeCount++] = cap;
+    onEnd[edgeCount] = u;
+    nextEdge[edgeCount] = firstEdge[v];
+    firstEdge[v] = edgeCount;
+    capacity[edgeCount++] = 0;
 }
 
-int findFlow(int u, int flow) {
-    if (u == destinationVertex) return flow; // возвращяем полученный минимум на пути
-    visited[u] = true;
-    for (int edge = firstEdge[u]; edge != -1; edge = nextEdge[edge]) {
-        int to = onEnd[edge];
+unsigned int findFlow(unsigned int u, unsigned int flow) {
+    if (u == destinationVertex) return flow;
+    visited[u] = 1;
+    for (unsigned int edge = firstEdge[u]; edge != -1; edge = nextEdge[edge]) {
+        unsigned int to = onEnd[edge];
         if (!visited[to] && capacity[edge] > 0) {
-            int minResult = findFlow(to, min(flow, capacity[edge])); // ищем поток в поддереве
-            if (minResult > 0) {                    // если нашли
-                capacity[edge]      -= minResult;   // у прямых ребер вычетаем поток
-                capacity[edge ^ 1]  += minResult;   // к обратным прибавляем
+            unsigned int minResult = findFlow(to, std::min(flow, capacity[edge]));
+            if (minResult > 0) {
+                capacity[edge]      -= minResult;
+                capacity[edge + 1]  += minResult;
                 return minResult;
             }
         }
     }
-    return 0; // если не нашли поток из этой вершины вернем 0
+    return 0;
 }
 
 int main() {
-    fill(firstEdge, firstEdge + MAX_VERTEX, -1);     // -1 означает ребра нет
-    cin >> numOfVertex >> numOfEdge;            // считываем количество вершин и ребер
+    std::fill(firstEdge, firstEdge + MAX_VERTEX, -1);
+    std::cin >> numOfVertex >> numOfEdge;
+	if (!numOfEdge || !numOfVertex) return 0;
 	sourceVertex = 1;
 	destinationVertex = numOfVertex;
-    for (int i = 0, u, v, cap; i < numOfEdge; i++) {
-        cin >> u >> v >> cap;
-        addEdge(u, v, cap);
+    for (unsigned int i = 0, u, v, weight; i < numOfEdge; i++) {
+        std::cin >> u >> v >> weight;
+        addEdge(u, v, weight);
     }
-    // Нахождение максимального потока
-    int maxFlow = 0;
-    int iterationResult = 0;
+    unsigned int maxFlow = 0;
+    unsigned int iterationResult = 0;
     while ((iterationResult = findFlow(sourceVertex, MAX_WEIGHT)) > 0) {
-        fill(visited, visited + MAX_VERTEX, false);
+        std::fill(visited, visited + MAX_VERTEX, 0);
         maxFlow += iterationResult;
     }
-    // Выводим максимальный поток
-    cout << maxFlow << endl;
+    std::cout << maxFlow << std::endl;
     return 0;
 }
